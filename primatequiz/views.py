bca4b2cad2e13e6c907ef62bb67ba41b25c72078
Andrew Barr <wabarr@gmail.com>
2014-02-09 10:53:31 -0600

    less offensive row colors

--- primatequiz/views.py
+++ primatequiz/views.py
@@ -11,9 +11,8 @@ def renderBase(request):
 
 def quiz(request):
     questions = question.objects.all()
-    pastels = ["#F7977A","#F9AD81","#FDC68A","#FFF79A","#C4DF9B","#A2D39C","#82CA9D","#7BCDC8","#6ECFF6","#7EA7D8","#8493CA","#A187BE","#BC8DBF","#F49AC2","#F6989D"]
     return render_to_response("splashpage.html",
-                                    {"questions":questions, "pastels":pastels},
+                                    {"questions":questions},
                                     context_instance=RequestContext(request))
 
 def results(request, slug=None):
