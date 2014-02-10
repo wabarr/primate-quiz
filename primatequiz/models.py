4282e5fb3eb83a36dcff2fa9201d8a2c76feacce
wabarr <wabarr@gmail.com>
2014-02-10 15:29:17 -0600

    attempt to add custom save method that shrinks image for mobile.  currently doesn't work

--- primatequiz/models.py
+++ primatequiz/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from PIL import Image
+from base.settings import MEDIA_ROOT
+import os
+import StringIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
 class species(models.Model):
@@ -8,12 +13,36 @@ class species(models.Model):
     description = models.TextField(max_length = 600, null=True, blank=True)
     funfact = models.CharField(max_length = 200,null=True, blank=True)
     pic = models.ImageField(upload_to="pics/%Y/%m/%d")
-
+    #picmobile = models.ImageField(upload_to="pics/%Y/%m/%d")
     class Meta:
         verbose_name_plural = "species"
 
     def __unicode__(self):
         return self.sciname
+
+# ##attempt to resize images for mobile
+#     def save(self):
+#         super(species, self).save()
+#         try:
+#             infile = os.path.join(MEDIA_ROOT, str(self.pic.name))
+#             outfile = infile.replace(".jpg", "-MOBILE.jpg")
+#             im = Image.open(infile)
+#             img_ratio = im.size[0] / float(im.size[1])
+#             thumb = im.thumbnail(700,700*img_ratio, Image.ANTIALIAS)
+#
+#             thumb_io = StringIO.StringIO()
+#             thumb.save(thumb, format='JPEG')
+#
+# # Create a new Django file-like object to be used in models as ImageField using
+# # InMemoryUploadedFile.  If you look at the source in Django, a
+# # SimpleUploadedFile is essentially instantiated similarly to what is shown here
+#             thumb_file = InMemoryUploadedFile(thumb_io, None, outfile, 'image/jpeg',
+#                                   thumb_io.len, None)
+#             self.picmobile = thumb_file
+#         except:
+#             print "cannot create thumbnail"
+#
+#         super(species, self).save()
 
 class question(models.Model):
     Q_text = models.CharField(max_length = 150,null=True, blank=True)
