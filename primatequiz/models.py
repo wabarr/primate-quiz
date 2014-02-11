5cac6b22bf8bd5ac618a7305b84bb86bb96f8715
wabarr <wabarr@gmail.com>
2014-02-11 10:24:12 -0600

    deleted extraneous models.py code

--- primatequiz/models.py
+++ primatequiz/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from PIL import Image
-from base.settings import MEDIA_ROOT
-import os
-import StringIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
+
 
 # Create your models here.
 class species(models.Model):
@@ -19,28 +15,3 @@ class species(models.Model):
 
     def __unicode__(self):
         return self.commonname + " (" + self.sciname + ")"
-
-# ##attempt to resize images for mobile
-#     def save(self):
-#         super(species, self).save()
-#         try:
-#             infile = os.path.join(MEDIA_ROOT, str(self.pic.name))
-#             outfile = infile.replace(".jpg", "-MOBILE.jpg")
-#             im = Image.open(infile)
-#             img_ratio = im.size[0] / float(im.size[1])
-#             thumb = im.thumbnail(700,700*img_ratio, Image.ANTIALIAS)
-#
-#             thumb_io = StringIO.StringIO()
-#             thumb.save(thumb, format='JPEG')
-#
-# # Create a new Django file-like object to be used in models as ImageField using
-# # InMemoryUploadedFile.  If you look at the source in Django, a
-# # SimpleUploadedFile is essentially instantiated similarly to what is shown here
-#             thumb_file = InMemoryUploadedFile(thumb_io, None, outfile, 'image/jpeg',
-#                                   thumb_io.len, None)
-#             self.picmobile = thumb_file
-#         except:
-#             print "cannot create thumbnail"
-#
-#         super(species, self).save()
-
