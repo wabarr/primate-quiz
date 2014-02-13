14415cec1b9639de01d2007563d844a014318da9
wabarr <wabarr@gmail.com>
2014-02-13 09:17:35 -0600

    added code to exclude google analytics tracking code for users with admin privlidges

--- primatequiz/views.py
+++ primatequiz/views.py
@@ -5,12 +5,11 @@ from primatequiz.models import *
 from questions.models import question
 
 
-
-# Create your views here.
-def renderBase(request):
-    return render_to_response("basic_bootstrap.html", context_instance=RequestContext(request))
-
 def quiz(request):
+    if request.user.has_perm("primatequiz.delete_species"):
+        useGoogleAnalytics =  False
+    else:
+        useGoogleAnalytics =  True
     questions = question.objects.order_by('?')
     if len(questions) < 5:
         questions = questions
@@ -18,10 +17,14 @@ def quiz(request):
         questions = questions[0:6]
     result = species.objects.order_by('?')[0]
     return render_to_response("splashpage.html",
-                                    {"questions":questions,"result":result},
+                                    {"questions":questions,"result":result,"useGoogleAnalytics":useGoogleAnalytics},
                                     context_instance=RequestContext(request))
 
 def results(request, slug=None):
+    if request.user.has_perm("primatequiz.delete_species"):
+        useGoogleAnalytics =  False
+    else:
+        useGoogleAnalytics =  True
 
     #If the get parameter indicates that this is from a facebook share, then redirect to the main page
     if request.GET.get("fromSocialMedia","") == "T":
@@ -34,6 +37,6 @@ def results(request, slug=None):
 
     if rez:
         return render_to_response("results.html",
-                                    {"species":rez},
+                                    {"species":rez,"useGoogleAnalytics":useGoogleAnalytics},
                                     context_instance=RequestContext(request))
 
